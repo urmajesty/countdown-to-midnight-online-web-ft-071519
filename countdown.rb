128fbbed30d55aa145f8b4913e8dba6ddb0e37e0
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 13:52:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,20 @@
+<<<<<<< HEAD
 def countdown(number)
   while number > 0 
   puts "#{number} SECOND(S)!"
   number -=1
   end
+=======
+
+
+def countdown (number)
+  countdown = number 
+  while countdown <= number
+  puts "#{number} SECOND(S)!"
+  number -=1
+  
+end
+>>>>>>> 0ddd70cf5f402c09b441c1d4e0633c311324742b
 return "HAPPY NEW YEAR!"
 end
 
